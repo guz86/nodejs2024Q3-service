ff016dfc8fdaf914ffb37a0b03035f3e111c4424
Pavel Gordienko <guzmo@yandex.ru>
2024-11-11 20:09:14 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as YAML from 'yamljs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const swaggerDocument = YAML.load('./doc/api.yaml');
+  SwaggerModule.setup('api-docs', app, swaggerDocument);
   const port = process.env.PORT || 4000;
   await app.listen(port);
+
 }
 bootstrap();
